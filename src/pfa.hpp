6cc40e4dd3386db1c0c84bac2a0d467d4769908f
Gao Wang <gaow@uchicago.edu>
2016-07-26 13:37:33 -0500

    Fix an error in likelihood calculation

--- src/pfa.hpp
+++ src/pfa.hpp
@@ -76,34 +76,30 @@ public:
     // this results in a N by k1k2 by q tensor of loglik
     // and a k1k2 by q by N tensor of delta
     // and a k1k2 by q matrix of pi_mat
-    arma::cube loglik_mat;
-    loglik_mat.set_size(int((F.n_rows - 1) * F.n_rows / 2), q.n_elem, D.n_rows);
 #pragma omp parallel for num_threads(n_threads)
     for (size_t qq = 0; qq < q.n_elem; qq++) {
       for (size_t k1 = 0; k1 < F.n_rows; k1++) {
         for (size_t k2 = 0; k2 < k1; k2++) {
           // given k1, k2 and q, density is a N-vector
           // in the end of the loop, the vector should populate a column of a slice of the tensor
-          arma::vec Dn_llik = arma::zeros<arma::vec>(D.n_rows);
           arma::vec Dn_delta = arma::zeros<arma::vec>(D.n_rows);
           for (size_t j = 0; j < D.n_cols; j++) {
             arma::vec density = D.col(j);
             double m = q.at(qq) * F.at(k2, j) + (1 - q.at(qq)) * F.at(k1, j);
             double sig = s.at(j);
-            Dn_llik += density.transform( [=](double x) { return (normal_pdf_log(x, m, sig)); } );
+            Dn_delta += density.transform( [=](double x) { return (normal_pdf_log(x, m, sig)); } );
           }
           size_t k1k2 = size_t(k1 * (k1 - 1) / 2 + k2);
           // populate the loglik and the delta tensor
           if (n_updates == std::make_pair(0, 0)) {
             // No update on pi_mat is available yet: approximate it under independence assumption
-            Dn_delta = arma::exp(Dn_llik) * (P.at(k1, k2) * omega.at(qq));
+            Dn_delta = arma::exp(Dn_delta) * (P.at(k1, k2) * omega.at(qq));
           }
           else {
-            Dn_delta = arma::exp(Dn_llik) * pi_mat.at(k1k2, qq);
+            Dn_delta = arma::exp(Dn_delta) * pi_mat.at(k1k2, qq);
           }
           // FIXME: this is slow, due to the cube/slice structure
           for (size_t n = 0; n < D.n_rows; n++) {
-            loglik_mat.slice(n).at(k1k2, qq) = Dn_llik.at(n);
             delta.slice(n).at(k1k2, qq) = Dn_delta.at(n);
           }
         }
@@ -114,8 +110,9 @@ public:
     loglik_vec.set_size(D.n_rows);
 #pragma omp parallel for num_threads(n_threads)
     for (size_t n = 0; n < D.n_rows; n++) {
-      delta.slice(n) = delta.slice(n) / arma::accu(delta.slice(n));
-      loglik_vec.at(n) = std::log(arma::accu(arma::exp(loglik_mat.slice(n))));
+      double sum_delta_n = arma::accu(delta.slice(n));
+      delta.slice(n) = delta.slice(n) / sum_delta_n;
+      loglik_vec.at(n) = std::log(sum_delta_n);
     }
     pi_mat = arma::sum(delta, 2) / D.n_rows;
     loglik = arma::accu(loglik_vec);
